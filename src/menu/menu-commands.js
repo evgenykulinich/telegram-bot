e2f033433b4858046e1552f587dfddd8d533ab8c
unknown <kulinichevgeny@gmail.com>
2023-07-16 21:16:07 +0300

    Minor improve

--- src/menu/menu-commands.js
+++ src/menu/menu-commands.js
@@ -1,5 +1,7 @@
 module.exports = {
   menuCommands: [
     { command: '/start', description: 'Start bot' },
+    { command: '/command 1', description: 'Do something' },
+    { command: '/command 2', description: 'Do something' },
   ]
 }
